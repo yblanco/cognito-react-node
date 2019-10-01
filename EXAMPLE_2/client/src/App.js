d750180c476b6e5cf808c7bc96d40650091b19d3
Yeinso Blanco <yeinsojbm@gmail.com>
2019-10-01 11:59:15 -0500

    removing app useless

--- EXAMPLE_2/client/src/App.js
+++ EXAMPLE_2/client/src/App.js
@@ -3,9 +3,6 @@ import Amplify from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react";
 import config from "./aws-exports";
 
-import logo from './logo.svg';
-import './App.css';
-
 Amplify.configure(config);
 
 
@@ -13,18 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Esta es una página
       </header>
     </div>
   );
